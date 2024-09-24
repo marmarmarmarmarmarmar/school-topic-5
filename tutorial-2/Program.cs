1d0c3ea9dbee629f38180e85d9c5120c5bb7179e
yesyes <hydrogenmelon@pm.me>
2024-09-24 09:39:03 -0400

    yeah!!!!!!!!!!

--- tutorial-2/Program.cs
+++ tutorial-2/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ui_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade;
+            Console.WriteLine("what was your grade?!??!?");
+            int.TryParse(Console.ReadLine(), out grade);
+            if (grade >= 50)
+            {
+                Console.WriteLine("congratulations!!!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("just like me fr");
+            }
+            if (grade < 50)
+            {
+                Console.WriteLine("You got an F!!!!!!!!!!!");
+            }
+            else if (grade <= 85)
+            {
+                Console.WriteLine("You got a B!!!!!!!!!!!");
+            }
+            else if (grade <= 75)
+            {
+                Console.WriteLine("You got a C!!!!!!!!!!!");
+            }
+            else if (grade <= 65)
+            {
+                Console.WriteLine("You got a D!!!!!!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("You got an A!!!!!!!!!!!");
+            }
+            int age;
+            Console.WriteLine("what is your age?!?!?!??");
+            int.TryParse(Console.ReadLine(), out age);
+            if (age >= 16)
+            {
+                Console.WriteLine("you can drive!!!!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("you can't drive!!!!!!!!!!");
+            }
+            Console.WriteLine("");
+            int bet;
+            Console.WriteLine("How much would you like to bet?!?1?1?!?");
+            if (int.TryParse(Console.ReadLine(), out bet))
+            {
+                Console.WriteLine("You bet " + bet.ToString("C") + "!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("thassa not a valid number!!!!!!!!!");
+                Console.WriteLine("Thus, your bet will be set to a minimum of $1.00!!!!");
+                bet = 1;
+            }
+            Console.WriteLine("");
+            int waterTemp;
+            Console.WriteLine("also, what is the temperature you would like to give to water??!?1");
+            if (int.TryParse(Console.ReadLine(), out waterTemp))
+            {
+                if (waterTemp <= 0)
+                {
+                    Console.WriteLine("the water at " + waterTemp + " celsius would be solid!!!!!");
+                }
+                else if (waterTemp <= 99)
+                {
+                    Console.WriteLine("the water at " + waterTemp + " celsius would be liquid!!!!!");
+                }
+                else
+                {
+                    Console.WriteLine("the water at " + waterTemp + " celsius would be a gas!!!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("that is not a valid temperature!!!!!!!!!!!!!!!!!!!!!!!");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("also what is your age again?!!?!?? I forgot!!!!!!!!!");
+            
+        }
+    }
+}
+
--- tutorial-2/obj/Debug/net8.0/tutorial-2.AssemblyInfo.cs
+++ tutorial-2/obj/Debug/net8.0/tutorial-2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tutorial-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd3d5d839c496a41be2a1fedb2e6690a1819019d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278eb957eb97bdd642b39f813f892a788cac062e")]
 [assembly: System.Reflection.AssemblyProductAttribute("tutorial-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tutorial-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
